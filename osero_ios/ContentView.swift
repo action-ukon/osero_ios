5999514bc9dcfdff48ae3f609c5280e98de304e6
action-ukon <actionukon0614mhf@gmail.com>
2022-01-12 22:40:03 +0900

    Initial Commit

---
+++ osero_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  osero_ios
+//
+//  Created by 坪田和樹 on 2022/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ osero_ios/osero_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  osero_iosApp.swift
+//  osero_ios
+//
+//  Created by 坪田和樹 on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct osero_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
