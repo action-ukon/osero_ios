381634cabb91a98b37d79b65762ab2f4bbbf6a23
action-ukon <actionukon0614mhf@gmail.com>
2022-01-13 21:25:12 +0900

    hashiyasume

--- osero_ios/ContentView.swift
+++ osero_ios/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var buttonCell = cellView()
+    
     var body: some View {
         ZStack {
             Color.black
@@ -16,7 +19,14 @@ struct ContentView: View {
                 .fill(Color.black)
                 .frame(width: 320, height: 320)
                 .border(Color.black, width: 1)
-            cellView()
+            
+            Button(action: {
+                buttonCell = Image("blackStone")
+                    .resizable()
+                    .frame(width: 40, height: 40) as! cellView
+            }){
+                buttonCell
+            }
         }
     }
 }
