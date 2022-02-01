6173bb343ee9b2f53e12128684955dcf51a5e969
action-ukon <actionukon0614mhf@gmail.com>
2022-02-01 23:26:27 +0900

    board turn such

--- osero_ios/arryBoard.swift
+++ osero_ios/arryBoard.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct arryBoard: View {
     @State var buttonCell = cellView()
     
-    let board: [[Int]] = [
-        [0,0,0,1,0,2,0,0],
+    @State var board: [[Int]] = [
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
         [0,0,0,0,0,0,0,0],
+        [0,0,0,2,1,0,0,0],
+        [0,0,0,1,2,0,0,0],
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0]
@@ -28,7 +28,11 @@ struct arryBoard: View {
                 HStack{
                     ForEach(0..<8) { i in
                         if(board[n][i] == 0){
-                            buttonCell.noStoneFunc()
+                            Button(action: {
+                                board[n][i] = 1
+                            }){
+                                buttonCell.noStoneFunc()
+                            }
                         } else if(board[n][i] == 1){
                             buttonCell.blackStoneFunc()
                         } else if(board[n][i] == 2){
